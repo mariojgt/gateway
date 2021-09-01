57e4f0577692eacb481bad5bdeb40c129b72b35c
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2021-09-01 17:45:23 +0200

    Update StripeController.php

--- src/Controllers/StripeController.php
+++ src/Controllers/StripeController.php
@@ -118,9 +118,6 @@ class StripeController extends Controller
             case 'invoice.paid':
                 $paymentIntent  = $event->data->object;
                 StripeUsePaymentSuccessEvent::dispatch($paymentIntent);
-            case 'invoice.finalized':
-                $paymentIntent  = $event->data->object;
-                StripeUsePaymentSuccessEvent::dispatch($paymentIntent);
             case 'customer.subscription.created':
                 $subscriptionSchedule = $event->data->object;
                 StripeUserSubscriptionSuccessEvent::dispatch($subscriptionSchedule);
