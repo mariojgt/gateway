eec9c50c418df710f141686c80fc082a67654843
server <server@dsmdesign.co.uk>
2021-09-29 15:17:38 +0100

    quick fix

--- src/Routes/gocardweb.php
+++ src/Routes/gocardweb.php
@@ -5,6 +5,7 @@ use Mariojgt\Gateway\Controllers\GocardlessController;
 
 // Gocardless weebhooks
 Route::group([
+    'middleware' => ['web'],
 ], function () {
     // Stripe webhooks
     Route::any('/gocardless/weebhook', [GocardlessController::class, 'webhookManager'])
