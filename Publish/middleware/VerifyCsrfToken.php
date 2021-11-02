36474d44181ae859925c51c0e5e6aa891152f69f
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2021-11-02 09:36:31 +0000

    Update VerifyCsrfToken.php

--- Publish/middleware/VerifyCsrfToken.php
+++ Publish/middleware/VerifyCsrfToken.php
@@ -8,7 +8,7 @@ class VerifyCsrfToken extends Middleware
 {
     /**
      * The URIs that should be excluded from CSRF verification.
-     *
+     * You need this for the weebhooks to work so the gateway can trigger the events
      * @var array
      */
     protected $except = [
